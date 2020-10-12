8824cd9df52fbe0e91ef9317bfefce848301405b
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-12 22:43:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,11 +10,3 @@ def join_nested_strings(src)
     element_index = 0
     phrase = []
     while element_index < src[row_index].length do
-      x =  src[row_index][element_index].to_s
-      if x.numeric?
-        src[row_index].shift
-      else
-        phrase << src[row_index][element_index]
-    end
-  phrase.join(" ")
-end
